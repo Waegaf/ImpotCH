3654cac8db19e4d9282f51c39cecbb201691826a
Waegaf <wael.gafaiti@gmail.com>
2025-12-15 15:36:01 +0100

    New test

---
+++ calculateur-app/tests/test_appError.py
@@ -0,0 +1,13 @@
+from streamlit.testing.v1 import AppTest
+import pytest
+
+
+
+def test_appError():
+    app_path = Path(__file__).resolve().parents[1] / "calculateur-app" / "app2.py"
+
+    at = AppTest.from_file(app_path)
+    at.run()
+
+
+    assert at.exception is None
